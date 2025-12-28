42ea0669edf6b7c7da890959de8be41b9161d926
l2oydchen <loydchen@outlook.com>
2025-12-29 02:22:49 +0800

    rename renderdoc.dll to renderdoc1.dll to skip some game's check, skip steamwebhelper.exe

--- renderdoccmd/renderdoccmd_win32.cpp
+++ renderdoccmd/renderdoccmd_win32.cpp
@@ -815,7 +815,7 @@ public:
     wchar_t rdocpath[1024];
 
     // fetch path to our matching renderdoc.dll
-    HMODULE rdoc = GetModuleHandleA("renderdoc.dll");
+    HMODULE rdoc = GetModuleHandleA("renderdoc1.dll");
 
     if(rdoc == NULL)
     {
